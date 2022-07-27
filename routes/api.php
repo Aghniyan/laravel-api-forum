5478bb07625271637e0bfe7bd0211c7fb87b4e4c
Aghniya Ni'amillah Nurhilman <aghniyan@gmail.com>
2022-07-27 14:49:28 +0700

    feat : add api store forums

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ForumController;
 
-Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
-    Route::post('register', [RegisterController::class , 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+Route::group(['middleware' => 'api'], function ($router) {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [RegisterController::class , 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
+    });
+
+    Route::apiResource('/forums', ForumController::class);
 });
